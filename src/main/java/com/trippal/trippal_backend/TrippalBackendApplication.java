e8ed29e35cf6a0a45bf367f81889d0c341f41e71
zh-deng <heromemo@web.de>
2025-04-26 21:02:12 +0200

    add boilerplate code

---
+++ src/main/java/com/trippal/trippal_backend/TrippalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trippal.trippal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrippalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrippalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trippal/trippal_backend/TrippalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trippal.trippal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrippalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
