6db1246b7306a2e76382f8ab1d81fbdd6b271032
zh-deng <heromemo@web.de>
2025-05-06 22:29:37 +0200

    add user repository

---
+++ src/main/java/com/trippal/trippal_backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.trippal.trippal_backend.repository;
+
+import com.trippal.trippal_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
