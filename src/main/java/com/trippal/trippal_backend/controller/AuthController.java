6b642635dc1e7258db912213f29f45f271715c10
zh-deng <heromemo@web.de>
2025-05-07 22:30:40 +0200

    add auth controller

---
+++ src/main/java/com/trippal/trippal_backend/controller/AuthController.java
@@ -0,0 +1,57 @@
+package com.trippal.trippal_backend.controller;
+
+import com.trippal.trippal_backend.model.User;
+import com.trippal.trippal_backend.repository.UserRepository;
+import com.trippal.trippal_backend.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtil jwtUtils;
+
+    @PostMapping("/signin")
+    public String authenticateUser(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getUsername(),
+                        user.getPassword()
+                )
+        );
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return jwtUtils.generateToken(userDetails.getUsername());
+    }
+
+    @PostMapping("/signup")
+    public String registerUser(@RequestBody User user) {
+        if(userRepository.existsByUsername(user.getUsername())) {
+            return "Error: Username is already taken!";
+        }
+
+        User newUser = new User(
+                null,
+                user.getUsername(),
+                encoder.encode(user.getPassword())
+        );
+        userRepository.save(newUser);
+        return "User registered successfully!";
+    }
+}
