37c3bf64b34477a79fbfa76c008bb2e47da1d345
zh-deng <heromemo@web.de>
2025-05-08 21:57:58 +0200

    add user info details class

---
+++ src/main/java/com/trippal/trippal_backend/service/UserInfoDetails.java
@@ -0,0 +1,63 @@
+package com.trippal.trippal_backend.service;
+
+
+import com.trippal.trippal_backend.entity.UserInfo;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserInfoDetails implements UserDetails {
+
+    private String username; // Changed from 'name' to 'email' for clarity
+    private String password;
+    private List<GrantedAuthority> authorities;
+
+    public UserInfoDetails(UserInfo userInfo) {
+        this.username = userInfo.getEmail(); // Use email as username
+        this.password = userInfo.getPassword();
+        this.authorities = List.of(userInfo.getRoles().split(","))
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password; // Return the password field
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
