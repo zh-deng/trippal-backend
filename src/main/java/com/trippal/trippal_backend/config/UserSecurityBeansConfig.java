ac8711f0c31ba6e005a685f3af6da4468cafbdb0
zh-deng <heromemo@web.de>
2025-05-08 21:59:47 +0200

    add user security beans config class

---
+++ src/main/java/com/trippal/trippal_backend/config/UserSecurityBeansConfig.java
@@ -0,0 +1,17 @@
+package com.trippal.trippal_backend.config;
+
+import com.trippal.trippal_backend.repository.UserInfoRepository;
+import com.trippal.trippal_backend.service.UserInfoService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserSecurityBeansConfig {
+
+    @Bean
+    public UserInfoService userInfoService(UserInfoRepository repository, PasswordEncoder encoder) {
+        return new UserInfoService(repository, encoder);
+    }
+}
