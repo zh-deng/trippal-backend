49c8407f319765f0b40d47aef871f71ab94daf53
zh-deng <heromemo@web.de>
2025-05-08 21:58:14 +0200

    modify user info repository

---
+++ src/main/java/com/trippal/trippal_backend/repository/UserInfoRepository.java
@@ -0,0 +1,14 @@
+package com.trippal.trippal_backend.repository;
+
+
+import com.trippal.trippal_backend.entity.UserInfo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email); // Use 'email' if that is the correct field for login
+}
