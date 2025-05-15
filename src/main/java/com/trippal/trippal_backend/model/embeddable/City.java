de222764ae7d557456ac4a506f3fbfe64efbe2bb
zh-deng <heromemo@web.de>
2025-05-15 22:59:25 +0200

    add city, country, attraction as embeddable classes

---
+++ src/main/java/com/trippal/trippal_backend/model/embeddable/City.java
@@ -0,0 +1,50 @@
+package com.trippal.trippal_backend.model.embeddable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Transient;
+
+@Embeddable
+public class City {
+
+    @Column(name = "city_name", nullable = false)
+    private String name;
+
+    @Column(name = "city_latitude", nullable = false)
+    private Double latitude;
+
+    @Column(name = "city_longitude", nullable = false)
+    private Double longitude;
+
+    public City() {}
+
+    public City(String name, Double latitude, Double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public Double getLatitude() { return latitude; }
+    public void setLatitude(Double latitude) { this.latitude = latitude; }
+
+    public Double getLongitude() { return longitude; }
+    public void setLongitude(Double longitude) { this.longitude = longitude; }
+
+    @Transient
+    @JsonProperty("coordinates")
+    public double[] getCoordinates() {
+        return new double[]{latitude, longitude};
+    }
+
+    @JsonProperty("coordinates")
+    public void setCoordinates(double[] coords) {
+        if (coords != null && coords.length == 2) {
+            this.latitude = coords[0];
+            this.longitude = coords[1];
+        }
+    }
+}
---
+++ src/main/java/com/trippal/trippal_backend/model/embeddable/Country.java
@@ -0,0 +1,56 @@
+package com.trippal.trippal_backend.model.embeddable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Transient;
+
+@Embeddable
+public class Country {
+
+    @Column(name = "country_name", nullable = false)
+    private String name;
+
+    @Column(name = "country_latitude", nullable = false)
+    private Double latitude;
+
+    @Column(name = "country_longitude", nullable = false)
+    private Double longitude;
+
+    @Column(name = "country_countryCode", nullable = false)
+    private String countryCode;
+
+    public Country() {}
+
+    public Country(String name, Double latitude, Double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public Double getLatitude() { return latitude; }
+    public void setLatitude(Double latitude) { this.latitude = latitude; }
+
+    public Double getLongitude() { return longitude; }
+    public void setLongitude(Double longitude) { this.longitude = longitude; }
+
+    public String getCountryCode() { return countryCode; }
+    public void setCountryCode(String countryCode) { this.countryCode = countryCode; }
+
+    @Transient
+    @JsonProperty("coordinates")
+    public double[] getCoordinates() {
+        return new double[]{latitude, longitude};
+    }
+
+    @JsonProperty("coordinates")
+    public void setCoordinates(double[] coords) {
+        if (coords != null && coords.length == 2) {
+            this.latitude = coords[0];
+            this.longitude = coords[1];
+        }
+    }
+}
