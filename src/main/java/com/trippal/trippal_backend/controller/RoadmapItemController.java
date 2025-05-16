2e44298104d1731d4bdc07381c509f524faae39c
zh-deng <heromemo@web.de>
2025-05-16 22:40:11 +0200

    add create roadmap item endpoint

--- src/main/java/com/trippal/trippal_backend/controller/RoadmapItemController.java
+++ src/main/java/com/trippal/trippal_backend/controller/RoadmapItemController.java
@@ -1,16 +1,19 @@
 package com.trippal.trippal_backend.controller;
 
+import com.trippal.trippal_backend.dtos.RoadmapItemDto;
 import com.trippal.trippal_backend.model.RoadmapItem;
-import com.trippal.trippal_backend.repository.RoadmapItemRepository;
 import com.trippal.trippal_backend.service.RoadmapItemService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/roadmap")
+@RequestMapping("/api/roadmapItem")
 public class RoadmapItemController {
 
     private final RoadmapItemService roadmapItemService;
@@ -20,8 +23,17 @@ public class RoadmapItemController {
     }
 
     @PostMapping
-    public ResponseEntity<RoadmapItem> create(@RequestBody RoadmapItem item) {
-        RoadmapItem saved = roadmapItemService.createRoadmapItem(item);
-        return ResponseEntity.ok(saved);
+    public ResponseEntity<RoadmapItemDto> createRoadmapItem(@RequestBody RoadmapItemDto item) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (userDetails != null) {
+            RoadmapItem createdRoadmapItem = roadmapItemService.createRoadmapItem(item);
+            RoadmapItemDto roadmapItemDto = new RoadmapItemDto(createdRoadmapItem.getId(), createdRoadmapItem.getTitle(), createdRoadmapItem.getDate(), createdRoadmapItem.getCountry(), createdRoadmapItem.getCity(), createdRoadmapItem.getAttraction(), createdRoadmapItem.getTrip().getId());
+
+            return ResponseEntity.ok(roadmapItemDto);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+
     }
 }
--- src/main/java/com/trippal/trippal_backend/service/RoadmapItemService.java
+++ src/main/java/com/trippal/trippal_backend/service/RoadmapItemService.java
@@ -1,20 +1,29 @@
 package com.trippal.trippal_backend.service;
 
+import com.trippal.trippal_backend.dtos.RoadmapItemDto;
 import com.trippal.trippal_backend.model.RoadmapItem;
+import com.trippal.trippal_backend.model.Trip;
 import com.trippal.trippal_backend.repository.RoadmapItemRepository;
+import com.trippal.trippal_backend.repository.TripRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoadmapItemService {
 
     private final RoadmapItemRepository roadmapItemRepository;
+    private final TripRepository tripRepository;
 
-    public RoadmapItemService(RoadmapItemRepository roadmapItemRepository) {
+    public RoadmapItemService(RoadmapItemRepository roadmapItemRepository, TripRepository tripRepository) {
         this.roadmapItemRepository = roadmapItemRepository;
+        this.tripRepository = tripRepository;
     }
 
-    public RoadmapItem createRoadmapItem(RoadmapItem item) {
-        return roadmapItemRepository.save(item);
+    public RoadmapItem createRoadmapItem(RoadmapItemDto roadmapItemDto) {
+        Trip trip = tripRepository.findById(roadmapItemDto.getTripId()).orElseThrow(() -> new RuntimeException("Trip not found"));
+
+        RoadmapItem roadmapItem = new RoadmapItem(roadmapItemDto.getTitle(), roadmapItemDto.getDate(), roadmapItemDto.getCountry(), roadmapItemDto.getCity(), roadmapItemDto.getAttraction(), trip);
+
+        return roadmapItemRepository.save(roadmapItem);
     }
 
 }
