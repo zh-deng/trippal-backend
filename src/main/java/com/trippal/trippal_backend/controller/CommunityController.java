2684ad787c3899dd63caa9c6d0d92d84d36848d3
zh-deng <heromemo@web.de>
2025-05-24 22:40:26 +0200

    implement community controller and service

--- src/main/java/com/trippal/trippal_backend/controller/CommunityController.java
+++ src/main/java/com/trippal/trippal_backend/controller/CommunityController.java
@@ -1,5 +1,33 @@
 package com.trippal.trippal_backend.controller;
 
+import com.trippal.trippal_backend.dtos.TripDto;
+import com.trippal.trippal_backend.model.Trip;
+import com.trippal.trippal_backend.service.CommunityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/community")
 public class CommunityController {
 
+    private final CommunityService communityService;
+
+    @Autowired
+    public CommunityController(CommunityService communityService) {
+        this.communityService = communityService;
+    }
+
+    @GetMapping
+    public Page<TripDto> getPublicTrips(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Page<Trip> tripPage = communityService.getPublicTrips(page, size);
+
+        return tripPage.map(TripDto::new);
+    }
 }
--- src/main/java/com/trippal/trippal_backend/service/CommunityService.java
+++ src/main/java/com/trippal/trippal_backend/service/CommunityService.java
@@ -1,4 +1,26 @@
 package com.trippal.trippal_backend.service;
 
+import com.trippal.trippal_backend.model.Trip;
+import com.trippal.trippal_backend.repository.TripRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommunityService {
+
+    private final TripRepository tripRepository;
+
+    @Autowired
+    public CommunityService(TripRepository tripRepository) {
+        this.tripRepository = tripRepository;
+    }
+
+    // Handles pagination of public trips
+    public Page<Trip> getPublicTrips(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return tripRepository.findByPublicTrue(pageable);
+    }
 }
